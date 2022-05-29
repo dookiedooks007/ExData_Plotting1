4c45b9bf8c7d478c0fa5cc4445640c88de37e50b
Amresh Dookran <99089510+dookiedooks007@users.noreply.github.com>
2022-05-29 14:54:05 -0400

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,19 @@
+#Reading in data from table
+data <- read.table("household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+summary(data)
+
+#Separating data from the dates 2007-02-01 and 2007-02-02.
+
+subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+
+globalActivePower <- as.numeric(subsetdata$Global_active_power)
+globalReactivePower <- as.numeric(subsetdata$Global_reactive_power)
+voltage <- as.numeric(subsetdata$Voltage)
+subMetering1 <- as.numeric(subsetdata$Sub_metering_1)
+subMetering2 <- as.numeric(subsetdata$Sub_metering_2)
+subMetering3 <- as.numeric(subsetdata$Sub_metering_3)
+
+#creating time series plot
+datetime <- strptime(paste(subsetdata$Date, subsetdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
